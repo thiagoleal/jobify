7afec144703f7541af3d080766bb9602f9a0cf1a
Thiago leal <thfleal@gmail.com>
2019-12-15 18:19:37 -0300

    Fixed views Fixed public

--- index.js
+++ index.js
@@ -9,8 +9,10 @@ const dbConnection = sqlite.open(path.resolve(__dirname, 'banco.sqlite'), { Prom
 
 const port = process.env.PORT || 3000;
 
+app.set('views', path.join(__dirname, 'views'));
+
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended: true}));
 
 //callback
